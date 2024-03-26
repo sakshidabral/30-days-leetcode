d951a40bde1c95e3c01fa123403e554da45f1336
sakshidabral <125342226+sakshidabral@users.noreply.github.com>
2024-03-26 21:28:31 +0530

    Time: 9 ms (57.10%) | Memory: 16.3 MB (19.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> m=matrix;
         int r=matrix.size(), c=matrix[0].size();
+        unordered_set<int> setRow;
+        unordered_set<int> setCol;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(matrix[i][j]==0){
-                    change(i,j,m);
+                    setRow.insert(i);
+                    setCol.insert(j);
                 }
             }
         }
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++)
-            swap(matrix[i][j],m[i][j]);
-        }
-    }
-    void change(int i, int j, vector<vector<int>>& m){
-        for(int r=0;r<m.size();r++){
-            m[r][j]=0;
-        }
-        for(int c=0;c<m[0].size();c++){
-            m[i][c]=0;
+            for(int j=0;j<c;j++){
+                if(setRow.count(i)>0 || setCol.count(j)>0)
+                matrix[i][j]=0;
+            }
         }
     }
 };
